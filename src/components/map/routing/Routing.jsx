88d9af951af2399b1d371a9fd5532c7e42c883b0
Dima <dimashikalov86@mail.ru>
2023-07-24 16:28:24 +0300

    added polyline

--- src/components/map/routing/Routing.jsx
+++ src/components/map/routing/Routing.jsx
@@ -5,28 +5,14 @@ import { useSelector } from "react-redux";
 import "leaflet-routing-machine/dist/leaflet-routing-machine.css";
 import "leaflet-routing-machine";
 
-// L.Marker.prototype.options.icon = L.icon({
-//   iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
-// });
-
 const Routing = () => {
   const wayPoints = useSelector((state) => state.maps.selectedWay);
 
   const map = useMap();
 
-  L.Icon.Default.mergeOptions({
-    iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
-    iconUrl: require("leaflet/dist/images/marker-icon.png"),
-    shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
-  });
-
   useEffect(() => {
     if (!wayPoints) return;
     if (!map) return;
-
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      attribution: "© OpenStreetMap contributors",
-    }).addTo(map);
 
     const routingControl = L.Routing.control({
       waypoints: [
